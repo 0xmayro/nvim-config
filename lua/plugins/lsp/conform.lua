ebdc81de77b82a3bc26e61d2d2b74d7224dad9a9
mayro <may.ronen9@gmail.com>
2023-12-11 18:45:53 +0200

    added a c and cpp formatter

--- lua/plugins/lsp/conform.lua
+++ lua/plugins/lsp/conform.lua
@@ -6,6 +6,8 @@ return {
 		require('conform').setup({
 			formatters_by_ft = {
 				lua = { 'stylua' },
+				c = { 'clang-format' },
+				cpp = { 'clang-format' },
 				python = { 'isort', 'black' },
 				javascript = { 'prettierd' },
 				typescript = { 'prettierd' },
