5e1823fb46a04d97fd33f1ac4b600a7f13c0225f
mayro <may.ronen9@gmail.com>
2023-12-17 20:59:56 +0200

    added os detection

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -3,17 +3,20 @@ return {
 	lazy = false,
 	version = '*',
 	config = function()
-		local powershell_options = {
-			shell = vim.fn.executable('pwsh') == 1 and 'pwsh' or 'powershell',
-			shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;',
-			shellredir = '-RedirectStandardOutput %s -NoNewWindow -Wait',
-			shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode',
-			shellquote = '',
-			shellxquote = '',
-		}
+		local is_win32 = vim.fn.has('win32')
+		if is_win32 == 1 then
+			local powershell_options = {
+				shell = vim.fn.executable('pwsh') == 1 and 'pwsh' or 'powershell',
+				shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;',
+				shellredir = '-RedirectStandardOutput %s -NoNewWindow -Wait',
+				shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode',
+				shellquote = '',
+				shellxquote = '',
+			}
 
-		for option, value in pairs(powershell_options) do
-			vim.opt[option] = value
+			for option, value in pairs(powershell_options) do
+				vim.opt[option] = value
+			end
 		end
 
 		require('toggleterm').setup({
