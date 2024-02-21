2c2fe2ec0802f28b562f4ca72719a15705f283d2
May Ronen <may.ronen9@gmail.com>
2024-02-21 17:48:09 +0200

    updated cmp settings

--- lua/plugins/lsp/cmp.lua
+++ lua/plugins/lsp/cmp.lua
@@ -9,7 +9,6 @@ return {
 		'L3MON4D3/LuaSnip',
 		'rafamadriz/friendly-snippets',
 		'saadparwaiz1/cmp_luasnip',
-		'onsails/lspkind.nvim',
 	},
 	config = function()
 		local cmp = require('cmp')
@@ -18,22 +17,19 @@ return {
 
 		cmp.setup({
 			snippet = {
-				-- REQUIRED - you must specify a snippet engine
 				expand = function(args)
 					require('luasnip').lsp_expand(args.body)
 				end,
 			},
-			window = {
-				-- completion = cmp.config.window.bordered(),
-				-- documentation = cmp.config.window.bordered(),
-			},
 			mapping = cmp.mapping.preset.insert({
+				['<C-n>'] = cmp.mapping.select_next_item(),
+				['<C-p>'] = cmp.mapping.select_prev_item(),
 				['<C-b>'] = cmp.mapping.scroll_docs(-4),
 				['<C-f>'] = cmp.mapping.scroll_docs(4),
 				['<C-Space>'] = cmp.mapping.complete(),
-				['<C-e>'] = cmp.mapping.abort(),
-				['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+				['<CR>'] = cmp.mapping.confirm({ select = true }),
 			}),
+
 			sources = cmp.config.sources({
 				{ name = 'nvim_lsp' },
 				{ name = 'luasnip' },
@@ -41,13 +37,6 @@ return {
 			}, {
 				{ name = 'buffer' },
 			}),
-			formatting = {
-				format = require('lspkind').cmp_format({
-					mode = 'symbol',
-					maxwidth = 50,
-					ellipsis_char = '...',
-				}),
-			},
 		})
 
 		-- Set configuration for specific filetype.
