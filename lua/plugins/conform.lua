b914dd574ac18723bce777be9dec8bb07836a3d7
May Ronen <may.ronen9@gmail.com>
2024-03-08 14:55:58 +0200

    fixed conform.nvim config

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,19 +1,23 @@
 return {
 	'stevearc/conform.nvim',
 	event = 'BufWritePre',
+	cmd = 'ConformInfo',
 	config = function()
 		require('conform').setup({
-			formmaters_by_ft = {
+			formatters_by_ft = {
 				lua = { 'stylua' },
 				python = { 'isort', 'black' },
 				javascript = { 'prettierd' },
+				javascriptreact = { 'prettierd' },
 				typescript = { 'prettierd' },
+				typescriptreact = { 'prettierd' },
 				css = { 'prettierd' },
 				html = { 'prettierd' },
 				svelte = { 'prettierd' },
 			},
 			format_on_save = {
-				timeout_ms = 500,
+				async = false,
+				timeout_ms = 1000,
 				lsp_fallback = true,
 			},
 		})
