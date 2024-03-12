c3fd4e03cc4bdefff1f5a1746688f081aad57ecf
May Ronen <may.ronen9@gmail.com>
2024-03-12 11:08:44 +0200

    moved conform to the lazy syntax

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,24 +2,22 @@ return {
 	'stevearc/conform.nvim',
 	event = 'BufWritePre',
 	cmd = 'ConformInfo',
-	config = function()
-		require('conform').setup({
-			formatters_by_ft = {
-				lua = { 'stylua' },
-				python = { 'isort', 'black' },
-				javascript = { { 'prettierd', 'prettier' } },
-				javascriptreact = { { 'prettierd', 'prettier' } },
-				typescript = { { 'prettierd', 'prettier' } },
-				typescriptreact = { { 'prettierd', 'prettier' } },
-				html = { { 'prettierd', 'prettier' } },
-				css = { { 'prettierd', 'prettier' } },
-				svelte = { { 'prettierd', 'prettier' } },
-			},
-			format_on_save = {
-				async = false,
-				timeout_ms = 1000,
-				lsp_fallback = true,
-			},
-		})
-	end,
+	opts = {
+		formatters_by_ft = {
+			lua = { 'stylua' },
+			python = { 'isort', 'black' },
+			javascript = { { 'prettierd', 'prettier' } },
+			javascriptreact = { { 'prettierd', 'prettier' } },
+			typescript = { { 'prettierd', 'prettier' } },
+			typescriptreact = { { 'prettierd', 'prettier' } },
+			html = { { 'prettierd', 'prettier' } },
+			css = { { 'prettierd', 'prettier' } },
+			svelte = { { 'prettierd', 'prettier' } },
+		},
+		format_on_save = {
+			async = false,
+			timeout_ms = 1000,
+			lsp_fallback = true,
+		},
+	},
 }
